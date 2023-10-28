4a3792ca6e38faa64fb521f220960bb0b897f311
OlgaYaritskaya <olga.yaritskaya@yandex.ru>
2023-10-28 20:45:57 +0300

    added JUnit 5

---
+++ src/test/java/SimpleJUnitTest.java
@@ -0,0 +1,21 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SimpleJUnitTest {
+    @BeforeAll
+    public static void init(){
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 4000;
+    }
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+    }
+}
