ef2adddeab286a9794618552382009ddedd0cf43
OlgaYaritskaya <olga.yaritskaya@yandex.ru>
2023-11-25 10:32:07 +0300

    changed to Chrome browser

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -9,8 +9,9 @@ import static com.codeborne.selenide.Selenide.*;
 public class BaseTest {
     @BeforeAll
     static void beforeAll() {
-        Configuration.browser = "firefox";
+       // Configuration.browser = "firefox";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
     }
