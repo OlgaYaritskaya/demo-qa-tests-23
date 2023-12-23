adc769f5ce0493d7837f068c474a0489d98c54b2
OlgaYaritskaya <olga.yaritskaya@yandex.ru>
2023-12-23 12:27:42 +0300

    added AfterEach Attaches

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,6 +3,7 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -32,9 +33,11 @@ public class BaseTest {
     }
 
     @AfterEach
-    public void afterEach() {
-        clearBrowserCookies();
-        clearBrowserLocalStorage();
-        closeWebDriver();
+    void addAttachments() {
+            Attach.screenshotAs("Last screenshot");
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
+            Attach.addVideo();
+            Selenide.closeWebDriver();
+        }
     }
-}
